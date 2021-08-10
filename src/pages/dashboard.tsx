9b1e45c6ee467321a7acdf6cd69a981f14fa62ed
ottonSam <ottonsam@gmail.com>
2021-08-10 00:03:43 -0300

    dashboard sidebar

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,7 +1,25 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+import { Sidebar } from "../components/Form/Sidebar";
 import { Header } from "../components/Header";
 
 export default function DashBoard() {
   return (
-    <Header />
+    <Flex
+      direction="column"
+      h="100vh"
+    >
+      <Header />
+
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+      </Flex>
+    </Flex>
   );
 }
