3d2b3cdd1235f7daf1dfb9ba8e8bab61cc4fe316
ottonSam <ottonsam@gmail.com>
2021-08-12 23:53:03 -0300

    remove old imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
 
 import { Input } from '../components/Form/Input'
 
