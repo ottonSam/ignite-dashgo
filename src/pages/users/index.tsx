3dd2aaa363dd808987ce146527d2a907dd6396da
ottonSam <ottonsam@gmail.com>
2021-08-12 23:58:53 -0300

    Primary structure for user list page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,23 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { Sidebar } from "../../components/Form/Sidebar";
+import { Header } from "../../components/Header";
+
 export default function userList() {
   return (
-    <h1>Users</h1>
+    <Box>
+      <Header />
+
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+
+        
+      </Flex>
+    </Box>
   );
 }
